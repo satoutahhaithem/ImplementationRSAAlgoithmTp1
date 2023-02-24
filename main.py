4de7123042562aa675497d9113f12d7b9b76ca3e
Satoutah Haithem <satoutahhaithem@gmail.com>
2023-02-24 22:28:31 +0100

    finish RSA algorithm

---
+++ main.py
@@ -0,0 +1,81 @@
+import random
+import math
+
+def generate_large_prime():
+    """
+    Generates a random prime number between 100 and 1000 (inclusive).
+    """
+    while True:
+        num = random.randint(500, 1000)
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            return num
+# generating the the two prime number 
+p=generate_large_prime()
+q=generate_large_prime()
+print("The p is ",p," The q is ",q)
+# the first part of our public key
+n=p*q
+# calculate phi_n (q-1)*(p-1)
+phi_n=(q-1)*(p-1)
+# we choose number 1<e<ph_n such gcd(e,ph_n)=1
+def get_second_part_public_key(phi_n):
+    while True:
+        e = random.randint(2, phi_n)
+        if math.gcd(e, phi_n) == 1:
+            return e
+
+e = get_second_part_public_key(phi_n)
+print ("The public key is ",n," the second part ",e)
+def inverse_modulaire(a,m):
+    for i in range (1,m):
+        if ( a * i) % m ==1:
+            return i
+    return None
+d=inverse_modulaire(e,phi_n)
+print ("the private key ",d)
+
+def convertToInt(myStr):
+    myStrInt=[ord(c) for c in myStr]
+    return myStrInt
+
+def convertToStr(myStrInt):
+    myStr=[chr(c) for c in myStrInt]
+    return myStr
+
+def appendArrayStr(arrStr):
+    return ''.join(arrStr)
+
+def encrypt(m, e, n):
+    arr=[]
+    for i in m:
+        arr.append(pow(i, e, n))
+    return arr
+
+
+message = "haithem"
+messageInt=convertToInt(message)
+print ("the message int is ",messageInt)
+
+
+cipherInt = encrypt(messageInt, e, n)
+print ("the cypher text",cipherInt)
+cipherText=convertToStr(cipherInt)
+print ("The cypher text in Table is ", cipherText," The cipher text merged",appendArrayStr(cipherText) )
+
+def decrypt(c, d, n):
+    arr=[]
+    for i in c :
+        arr.append(pow(i, d, n))
+    return arr
+
+plainInt = decrypt(cipherInt, d, n)
+print("the plain Int",plainInt)
+plainText=''.join(convertToStr(plainInt))
+print ("the plain text is ",plainText)
+
+
+
+
